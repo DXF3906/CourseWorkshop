1625ee6b686fc0ad148de887061baabaae5a880d
DXF3906 <1778027321@qq.com>
2016-07-06 19:49:47 +0800

    侧滑效果初步

---
+++ app/src/main/java/com/qianfeng/courseworkshop/coursefragment/CourseLeftFragment.java
@@ -0,0 +1,34 @@
+package com.qianfeng.courseworkshop.coursefragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.qianfeng.courseworkshop.R;
+
+/**
+ * 课程对应的侧滑界面
+ * fdb
+ * Created by asus on 2016/7/6.
+ */
+
+public class CourseLeftFragment extends Fragment {
+    private FragmentActivity activity;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        activity = getActivity();
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.course_left_fragment, null);
+
+        return view;
+    }
+
+}
