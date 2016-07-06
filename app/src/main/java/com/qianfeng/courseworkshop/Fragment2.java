fb464645b3c81b43fbebf0d73d8f3fb9e20ad75f
Administrator <sudazheng@qq.com>
2016-07-06 12:09:26 +0800

    主界面

---
+++ app/src/main/java/com/qianfeng/courseworkshop/Fragment2.java
@@ -0,0 +1,27 @@
+package com.qianfeng.courseworkshop;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment2 extends Fragment {
+    private FragmentActivity activity;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        activity = getActivity();
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, null);
+
+
+        return view;
+    }
+}
--- app/src/main/java/com/qianfeng/courseworkshop/MainActivity.java
+++ app/src/main/java/com/qianfeng/courseworkshop/MainActivity.java
@@ -10,41 +10,47 @@ package com.qianfeng.courseworkshop;
 
 
 public class MainActivity extends AppCompatActivity {
-    private FragmentManager supportFragmentManager;
-    private FragmentTransaction transaction;
     private Fragment fragment1;
+    private Fragment fragment2;
     private RadioGroup rg_main_id;
-
+    private FragmentManager supportFragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        supportFragmentManager = getSupportFragmentManager();
         rg_main_id=(RadioGroup)findViewById(R.id.rg_main_id);
         fragment1=new Fragment1();
-        supportFragmentManager = getSupportFragmentManager();
-         transaction = supportFragmentManager
-                .beginTransaction();
-        transaction.replace(R.id.ll_content_id, fragment1).commit();
+        fragment2=new Fragment2();
+        replaceContainerWidget(fragment1);
         ((RadioButton)rg_main_id.getChildAt(0)).setChecked(true);
         rg_main_id.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
               if(((RadioButton)radioGroup.getChildAt(0)).isChecked()){
-                  transaction.replace(R.id.ll_content_id, fragment1).commit();
+                  replaceContainerWidget(fragment1);
               }
-//              if(((RadioButton)radioGroup.getChildAt(1)).isChecked()){
-//                  transaction.replace(R.id.ll_content_id, fragment1).commit();
-//              }
-//              if(((RadioButton)radioGroup.getChildAt(2)).isChecked()){
-//                  transaction.replace(R.id.ll_content_id, fragment1).commit();
-//              }
-//              if(((RadioButton)radioGroup.getChildAt(3)).isChecked()){
-//                  transaction.replace(R.id.ll_content_id, fragment1).commit();
-//              }
-//              if(((RadioButton)radioGroup.getChildAt(4)).isChecked()){
-//                  transaction.replace(R.id.ll_content_id, fragment1).commit();
-//              }
+              if(((RadioButton)radioGroup.getChildAt(1)).isChecked()){
+                  replaceContainerWidget(fragment2);
+              }
+              if(((RadioButton)radioGroup.getChildAt(2)).isChecked()){
+                  replaceContainerWidget(fragment2);
+              }
+              if(((RadioButton)radioGroup.getChildAt(3)).isChecked()){
+                  replaceContainerWidget(fragment2);
+              }
+              if(((RadioButton)radioGroup.getChildAt(4)).isChecked()){
+                  replaceContainerWidget(fragment2);
+              }
             }
         });
+    }
+    private void replaceContainerWidget(Fragment fragment) {
+        FragmentTransaction transaction = supportFragmentManager
+                .beginTransaction();
+        transaction.replace(R.id.ll_content_id, fragment);
+
+        transaction.commit();
+
     }
 }
