f5d9e4123106537c244ab6c01010c2a0955e6adb
Administrator <sudazheng@qq.com>
2016-07-06 11:59:17 +0800

    主界面

---
+++ app/src/main/java/com/qianfeng/courseworkshop/Fragment1.java
@@ -0,0 +1,29 @@
+package com.qianfeng.courseworkshop;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment1 extends Fragment {
+    private FragmentActivity activity;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        activity = getActivity();
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, null);
+
+
+        return view;
+    }
+}
