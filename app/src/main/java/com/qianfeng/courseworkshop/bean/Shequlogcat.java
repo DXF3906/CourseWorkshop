1daa9c3a5a1c06625bceee7fed47e794a000e051
Administrator <sudazheng@qq.com>
2016-07-11 19:31:54 +0800

    新建社区实体类

---
+++ app/src/main/java/com/qianfeng/courseworkshop/bean/Shequlogcat.java
@@ -0,0 +1,185 @@
+package com.qianfeng.courseworkshop.bean;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 16-7-11.
+ */
+public class Shequlogcat {
+
+    /**
+     * code : 1
+     * errorType : 0
+     * list : [{"name":"1F Ke星广场","id":"947","pid":"0","hasAnswer":0,"img":"","child":[{"name":"新手报到","id":"1011","pid":"947","hasAnswer":0,"newPost":"6036","img":"http://www.kgc.cn/upload/public/1433251576418864.png"},{"name":"新课来了","id":"1158","pid":"947","hasAnswer":0,"newPost":"189","img":"http://assets.kgc.cn/upload/forum/20160527/1464346984460461.png"},{"name":"有奖活动","id":"1029","pid":"947","hasAnswer":0,"newPost":"13388","img":"http://www.kgc.cn/upload/public/1433295486203969.png"},{"name":"提个建议","id":"1008","pid":"947","hasAnswer":0,"newPost":"3515","img":"http://www.kgc.cn/upload/public/1433251374303698.png"},{"name":"大事记","id":"1028","pid":"947","hasAnswer":0,"newPost":"1945","img":"http://www.kgc.cn/upload/public/1443092446886887.png"},{"name":"灌水贴图","id":"1005","pid":"947","hasAnswer":0,"newPost":"9734","img":"http://assets.kgc.cn/upload/forum/20160531/1464655228677371.png"},{"name":"设计大赛","id":"1153","pid":"947","hasAnswer":0,"newPost":"3693","img":"http://assets.kgc.cn/upload/forum/20160509/1462787093629272.png"}]},{"name":"2F 资源分享","id":"948","pid":"0","hasAnswer":0,"img":"","child":[{"name":"软件下载","id":"1010","pid":"948","hasAnswer":0,"newPost":"3931","img":"http://www.kgc.cn/upload/public/1433251560549699.png"},{"name":"电子书籍","id":"1009","pid":"948","hasAnswer":0,"newPost":"5348","img":"http://www.kgc.cn/upload/public/1433251353937394.png"},{"name":"精选题库","id":"1012","pid":"948","hasAnswer":0,"newPost":"1177","img":"http://www.kgc.cn/upload/public/1433251627915922.png"},{"name":"互联网资讯","id":"1006","pid":"948","hasAnswer":0,"newPost":"2265","img":"http://www.kgc.cn/upload/public/1433251329362402.png"}]},{"name":"3F UI设计学院","id":"1098","pid":"0","hasAnswer":0,"img":"","child":[{"name":"UI设计学院主版","id":"979","pid":"1098","hasAnswer":0,"newPost":"4567","img":"http://www.kgc.cn/upload/public/1433253379645811.png"},{"name":"APP设计","id":"1164","pid":"1098","hasAnswer":1,"newPost":"254","img":"http://assets.kgc.cn/upload/forum/20160527/1464345807334855.png"},{"name":"交互设计","id":"1165","pid":"1098","hasAnswer":1,"newPost":"210","img":"http://assets.kgc.cn/upload/forum/20160527/1464345832352583.png"},{"name":"PS","id":"1166","pid":"1098","hasAnswer":1,"newPost":"380","img":"http://assets.kgc.cn/upload/forum/20160527/1464345843895770.png"},{"name":"AI","id":"1167","pid":"1098","hasAnswer":1,"newPost":"156","img":"http://assets.kgc.cn/upload/forum/20160527/1464346947500742.png"},{"name":"网页设计","id":"983","pid":"1098","hasAnswer":1,"newPost":"1308","img":"http://www.kgc.cn/upload/public/1433253311687684.png"}]},{"name":"4F 前端开发学院","id":"1097","pid":"0","hasAnswer":0,"img":"","child":[{"name":"前端开发学院主版","id":"967","pid":"1097","hasAnswer":0,"newPost":"2476","img":"http://www.kgc.cn/upload/public/1433253215183670.png"},{"name":"HTML5","id":"963","pid":"1097","hasAnswer":1,"newPost":"2280","img":"http://www.kgc.cn/upload/public/1433253181584862.png"},{"name":"JavaScript","id":"1172","pid":"1097","hasAnswer":1,"newPost":"452","img":"http://assets.kgc.cn/upload/forum/20160527/1464345728834700.png"},{"name":"bootstrap","id":"1173","pid":"1097","hasAnswer":1,"newPost":"156","img":"http://assets.kgc.cn/upload/forum/20160527/1464345740858166.png"},{"name":"jQueryMobile","id":"1174","pid":"1097","hasAnswer":1,"newPost":"177","img":"http://assets.kgc.cn/upload/forum/20160527/1464345778927663.png"}]},{"name":"5F 大数据学院","id":"950","pid":"0","hasAnswer":0,"img":"","child":[{"name":"大数据学院主版","id":"965","pid":"950","hasAnswer":0,"newPost":"1070","img":"http://www.kgc.cn/upload/public/1433253196600203.png"},{"name":"Java","id":"951","pid":"950","hasAnswer":1,"newPost":"14852","img":"http://www.kgc.cn/upload/public/1433252981333976.png"},{"name":"Android","id":"959","pid":"950","hasAnswer":1,"newPost":"4087","img":"http://www.kgc.cn/upload/public/1433253149580649.png"},{"name":"spark","id":"1159","pid":"950","hasAnswer":1,"newPost":"96","img":"http://assets.kgc.cn/upload/forum/20160527/1464345522903134.png"},{"name":"Hadoop","id":"1160","pid":"950","hasAnswer":1,"newPost":"148","img":"http://assets.kgc.cn/upload/forum/20160527/1464345562150720.png"},{"name":"MySQL","id":"1161","pid":"950","hasAnswer":1,"newPost":"861","img":"http://assets.kgc.cn/upload/forum/20160527/1464345574439240.png"},{"name":"Mybatis","id":"1162","pid":"950","hasAnswer":0,"newPost":"208","img":"http://assets.kgc.cn/upload/forum/20160527/1464345661861948.png"},{"name":"spring","id":"1163","pid":"950","hasAnswer":0,"newPost":"164","img":"http://assets.kgc.cn/upload/forum/20160527/1464345687748579.png"}]},{"name":"6F 云计算学院","id":"1099","pid":"0","hasAnswer":0,"img":"","child":[{"name":"云计算学院主版","id":"987","pid":"1099","hasAnswer":0,"newPost":"1143","img":"http://www.kgc.cn/upload/public/1433253274774591.png"},{"name":"Linux","id":"985","pid":"1099","hasAnswer":1,"newPost":"1609","img":"http://assets.kgc.cn/upload/forum/20160527/1464345212538342.png"},{"name":"Windows","id":"1169","pid":"1099","hasAnswer":1,"newPost":"241","img":"http://assets.kgc.cn/upload/forum/20160527/1464345370928374.png"},{"name":"shell","id":"1170","pid":"1099","hasAnswer":1,"newPost":"985","img":"http://assets.kgc.cn/upload/forum/20160527/1464345383144548.png"},{"name":"oracle","id":"1171","pid":"1099","hasAnswer":1,"newPost":"442","img":"http://assets.kgc.cn/upload/forum/20160527/1464345407424378.png"}]},{"name":"7F 热门技术","id":"1100","pid":"0","hasAnswer":0,"img":"","child":[{"name":"Python","id":"953","pid":"1100","hasAnswer":1,"newPost":"1242","img":"http://www.kgc.cn/upload/public/1433253033878686.png"},{"name":"C","id":"955","pid":"1100","hasAnswer":1,"newPost":"1209","img":"http://www.kgc.cn/upload/public/1433253057716709.png"},{"name":".Net","id":"957","pid":"1100","hasAnswer":1,"newPost":"1279","img":"http://www.kgc.cn/upload/public/1433253100526162.png"},{"name":"PHP","id":"961","pid":"1100","hasAnswer":1,"newPost":"1239","img":"http://www.kgc.cn/upload/public/1433253165133751.png"},{"name":"ios","id":"1143","pid":"1100","hasAnswer":1,"newPost":"741","img":"http://assets.kgc.cn/upload/forum/20160317/1458178489514033.png"},{"name":"数据库","id":"969","pid":"1100","hasAnswer":1,"newPost":"656","img":"http://www.kgc.cn/upload/public/1433253231112015.png"},{"name":"软件工程","id":"973","pid":"1100","hasAnswer":1,"newPost":"320","img":"http://www.kgc.cn/upload/public/1433253282929425.png"},{"name":"电商","id":"975","pid":"1100","hasAnswer":1,"newPost":"1319","img":"http://www.kgc.cn/upload/public/1433253300106067.png"},{"name":"数据结构","id":"1175","pid":"1100","hasAnswer":1,"newPost":"1","img":"http://assets.kgc.cn/upload/forum/20160527/1464346142841880.png"},{"name":"LoadRunner","id":"1176","pid":"1100","hasAnswer":1,"newPost":"73","img":"http://assets.kgc.cn/upload/forum/20160527/1464346157173772.png"},{"name":"测试基础","id":"1177","pid":"1100","hasAnswer":1,"newPost":"449","img":"http://assets.kgc.cn/upload/forum/20160527/1464346171336110.png"},{"name":"UFT","id":"1178","pid":"1100","hasAnswer":1,"newPost":"11","img":"http://assets.kgc.cn/upload/forum/20160527/1464346185873856.png"},{"name":"微信营销","id":"1179","pid":"1100","hasAnswer":1,"newPost":"686","img":"http://assets.kgc.cn/upload/forum/20160527/1464346200774380.png"},{"name":"社群营销","id":"1180","pid":"1100","hasAnswer":1,"newPost":"1208","img":"http://assets.kgc.cn/upload/forum/20160527/1464346215381195.png"},{"name":"SEO","id":"1181","pid":"1100","hasAnswer":1,"newPost":"1070","img":"http://assets.kgc.cn/upload/forum/20160527/1464346236428826.png"},{"name":"SEM","id":"1182","pid":"1100","hasAnswer":1,"newPost":"87","img":"http://assets.kgc.cn/upload/forum/20160527/1464346269175565.png"},{"name":"C#","id":"1183","pid":"1100","hasAnswer":1,"newPost":"51","img":"http://assets.kgc.cn/upload/forum/20160527/1464346287101177.png"},{"name":"swift","id":"1184","pid":"1100","hasAnswer":1,"newPost":"32","img":"http://assets.kgc.cn/upload/forum/20160527/1464346305116582.png"},{"name":"VR","id":"1185","pid":"1100","hasAnswer":1,"newPost":"650","img":"http://assets.kgc.cn/upload/forum/20160527/1464346319796137.png"},{"name":"AR","id":"1186","pid":"1100","hasAnswer":1,"newPost":"329","img":"http://assets.kgc.cn/upload/forum/20160527/1464347017820349.png"}]},{"name":"8F 其他","id":"1106","pid":"0","hasAnswer":0,"img":"http://assets.kgc.cn/upload/partition/20160314/1457922114797285.png","child":[{"name":"二级C语言","id":"1107","pid":"1106","hasAnswer":1,"newPost":"76","img":"http://assets.kgc.cn/upload/forum/20160314/1457922373213105.png"},{"name":"二级MS office","id":"1108","pid":"1106","hasAnswer":1,"newPost":"123","img":"http://assets.kgc.cn/upload/forum/20160314/1457922432434435.png"},{"name":"办公软件","id":"1146","pid":"1106","hasAnswer":0,"newPost":"37","img":"http://assets.kgc.cn/upload/forum/20160317/1458180499367901.png"},{"name":"职业素养","id":"993","pid":"1106","hasAnswer":0,"newPost":"1204","img":"http://www.kgc.cn/upload/public/1433253913402512.png"},{"name":"课说新语","id":"995","pid":"1106","hasAnswer":1,"newPost":"977","img":"http://www.kgc.cn/upload/public/1433253091144542.png"},{"name":"职业规划","id":"997","pid":"1106","hasAnswer":1,"newPost":"1382","img":"http://www.kgc.cn/upload/public/1433253183661571.png"}]}]
+     * iVersion : 3.5
+     */
+
+    private int code;
+    private int errorType;
+    private String iVersion;
+    /**
+     * name : 1F Ke星广场
+     * id : 947
+     * pid : 0
+     * hasAnswer : 0
+     * img :
+     * child : [{"name":"新手报到","id":"1011","pid":"947","hasAnswer":0,"newPost":"6036","img":"http://www.kgc.cn/upload/public/1433251576418864.png"},{"name":"新课来了","id":"1158","pid":"947","hasAnswer":0,"newPost":"189","img":"http://assets.kgc.cn/upload/forum/20160527/1464346984460461.png"},{"name":"有奖活动","id":"1029","pid":"947","hasAnswer":0,"newPost":"13388","img":"http://www.kgc.cn/upload/public/1433295486203969.png"},{"name":"提个建议","id":"1008","pid":"947","hasAnswer":0,"newPost":"3515","img":"http://www.kgc.cn/upload/public/1433251374303698.png"},{"name":"大事记","id":"1028","pid":"947","hasAnswer":0,"newPost":"1945","img":"http://www.kgc.cn/upload/public/1443092446886887.png"},{"name":"灌水贴图","id":"1005","pid":"947","hasAnswer":0,"newPost":"9734","img":"http://assets.kgc.cn/upload/forum/20160531/1464655228677371.png"},{"name":"设计大赛","id":"1153","pid":"947","hasAnswer":0,"newPost":"3693","img":"http://assets.kgc.cn/upload/forum/20160509/1462787093629272.png"}]
+     */
+
+    private List<ListBean> list;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public int getErrorType() {
+        return errorType;
+    }
+
+    public void setErrorType(int errorType) {
+        this.errorType = errorType;
+    }
+
+    public String getIVersion() {
+        return iVersion;
+    }
+
+    public void setIVersion(String iVersion) {
+        this.iVersion = iVersion;
+    }
+
+    public List<ListBean> getList() {
+        return list;
+    }
+
+    public void setList(List<ListBean> list) {
+        this.list = list;
+    }
+
+    public static class ListBean {
+        private String name;
+        private String id;
+        private String pid;
+        private int hasAnswer;
+        private String img;
+        /**
+         * name : 新手报到
+         * id : 1011
+         * pid : 947
+         * hasAnswer : 0
+         * newPost : 6036
+         * img : http://www.kgc.cn/upload/public/1433251576418864.png
+         */
+
+        private List<ChildBean> child;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getPid() {
+            return pid;
+        }
+
+        public void setPid(String pid) {
+            this.pid = pid;
+        }
+
+        public int getHasAnswer() {
+            return hasAnswer;
+        }
+
+        public void setHasAnswer(int hasAnswer) {
+            this.hasAnswer = hasAnswer;
+        }
+
+        public String getImg() {
+            return img;
+        }
+
+        public void setImg(String img) {
+            this.img = img;
+        }
+
+        public List<ChildBean> getChild() {
+            return child;
+        }
+
+        public void setChild(List<ChildBean> child) {
+            this.child = child;
+        }
+
+        public static class ChildBean {
+            private String name;
+            private String id;
+            private String pid;
+            private int hasAnswer;
+            private String newPost;
+            private String img;
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public String getId() {
+                return id;
+            }
+
+            public void setId(String id) {
+                this.id = id;
+            }
+
+            public String getPid() {
+                return pid;
+            }
+
+            public void setPid(String pid) {
+                this.pid = pid;
+            }
+
+            public int getHasAnswer() {
+                return hasAnswer;
+            }
+
+            public void setHasAnswer(int hasAnswer) {
+                this.hasAnswer = hasAnswer;
+            }
+
+            public String getNewPost() {
+                return newPost;
+            }
+
+            public void setNewPost(String newPost) {
+                this.newPost = newPost;
+            }
+
+            public String getImg() {
+                return img;
+            }
+
+            public void setImg(String img) {
+                this.img = img;
+            }
+        }
+    }
+}
